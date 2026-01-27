05d991e43eb6afd5d14cba63537ecbc5cca39a61
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-01-28 00:55:18 +0530

    api dataset done

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Dataset(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file_name = models.CharField(max_length=255)
+
+    total_equipment = models.IntegerField()
+    avg_flowrate = models.FloatField()
+    avg_pressure = models.FloatField()
+    avg_temperature = models.FloatField()
+
+    type_distribution = models.JSONField()
+
+    def __str__(self):
+        return f"{self.file_name} ({self.uploaded_at.date()})"
